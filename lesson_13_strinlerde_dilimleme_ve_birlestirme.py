b8d434b61c9727207f65e676156e01d5570fcdaf
Devran Acar <devranacar246@gmail.com>
2026-01-10 01:29:26 +0300

    stringlerde dilimleme ve birlestirme ögrenildi

---
+++ lesson_13_strinlerde_dilimleme_ve_birlestirme.py
@@ -0,0 +1,15 @@
+"""Stringlerde dilimleme"""
+
+# text = "pyton is weird"
+
+# print(text[1:5]) # 5'in dahil olmadığına dikkat!
+# print(text[-5:-1]) # - koyduğumuz zaman okuma sağdan sola doğru olur ve bu seferde soldaki değer dahil edilmez
+
+
+
+"""Stringlerde birleştirme"""
+
+text = "python"
+text2 = "is weird"
+result = text+" "+text2
+print(result)
